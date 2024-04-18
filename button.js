12c429b8223a283af04669c9dd883a843e2c5cce
Jay-Kip <hanningtonkiptoo14@gmail.com>
2024-04-19 01:45:48 +0300

    Script that automates clicking of a button

---
+++ button.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
+
+async function start() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://learnwebcode.github.io/practice-requests/")
+    
+    const names = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent)
+    })
+    await fs.writeFile("names.txt", names.join("\r\n"))
+
+
+    await page.click("#clickme")
+    const clickedData = await page.$eval("#data", el => el.textContent)
+    console.log(clickedData)
+
+    const photos = await page.$$eval("img", (imgs) => {
+        return imgs.map(x => start.src)
+
+    })
+
+    for (const photo of photos) {
+        const imagepage = await page.goto(photo)
+        await fs.writeFile(photo.split("/").pop(), await imagepage.buffer())
+    }
+
+
+
+    await browser.close()
+}
+
+start()
